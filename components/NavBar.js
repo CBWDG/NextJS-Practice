763214b0cf4e34bc8e2c62d12a699bad1167faa8
CBWDG <zaratole@naver.com>
2023-08-24 17:53:42 +0900

    nomardCoder1.5/Styles JSX;text-decoration해결 요망

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,16 +1,24 @@
-import Link from "next/link"
-import { useRouter } from "next/router"
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
     const router = useRouter();
     return <nav>
-        <Link style={{color: router.pathname === "/" ? "red" : "blue"}} href="/">
-            Home
+        <Link href="/">
+            <span>Home</span>
         </Link>
         <br></br>
-        <Link style={{color: router.pathname === "/about" ? "red" : "blue"}} href="/about">
-            About
+        <Link href="/about">
+            <span>About</span>
         </Link>
-        
+        <style jsx>{` 
+            nav {
+                background-color: tomato;
+            }
+            span {
+                text-decoration: none;
+            }
+        `}
+        </style>
     </nav>
 }
